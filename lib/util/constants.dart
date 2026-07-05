d3d62033f77d85cbfce3a6922c8b124616306f07
muhammad hilmi <hilmihisham@live.com>
2026-07-05 22:42:42 +0800

    #36 adding app colors into constants

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/material.dart';
 
 class Constants {
   static const appVersion = _AppVersion();
   static const csvHeader = _CsvHeader();
+  static const appColors = _AppColors();
 }
 
 class _AppVersion {
@@ -18,4 +20,19 @@ class _CsvHeader {
   final String column2 = 'tsundoku';
   final String column3 = 'aolabs';
   final String column4 = '0';
+}
+
+class _AppColors {
+  // color name from coolors.co
+  const _AppColors();
+  
+  Color get red1 => Colors.red.shade400; // strawberry red
+  Color get red2 => const Color(0xFFF6A3A2); // powder blush
+
+  Color get yellow1 => Colors.amber; // amber gold
+  Color get yellow2 => const Color(0xFFFFE699); // jasmine
+  
+  Color get green1 => Colors.green.shade400; // moss green
+  Color get green2 => const Color(0xFFB9DFBA); // celadon
+
 }
